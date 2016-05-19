75d4abcc88e89b4bbda9ae5fe4e90be129d5debd
azmikamis <azmikamis82@gmail.com>
2016-05-20 01:47:13 +0800

    Added global filters

--- MvcApp/Global.asax.cs
+++ MvcApp/Global.asax.cs
@@ -13,6 +13,7 @@ namespace MvcApp
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
         }
     }
 }
